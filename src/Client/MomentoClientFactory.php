770db7328e9b26400a484e6f11838286cc5c05ee
Pete Gautier <pete@momentohq.com>
2023-10-18 09:52:27 -0700

    feat: use a single cache for all bins

--- src/Client/MomentoClientFactory.php
+++ src/Client/MomentoClientFactory.php
@@ -22,30 +22,12 @@ class MomentoClientFactory {
         $settings = Settings::get('momento_cache', []);
         $authToken = array_key_exists('api_token', $settings) ?
             $settings['api_token'] : getenv("MOMENTO_API_TOKEN");
-        $this->cachePrefix = array_key_exists('cache_name_prefix', $settings) ?
-            $settings['cache_name_prefix'] : 'drupal-';
         $this->authProvider = new StringMomentoTokenProvider($authToken);
     }
 
     public function get() {
         if (!$this->client) {
             $this->client = new CacheClient(Laptop::latest(), $this->authProvider, 30);
-            // Ensure "container" cache exists
-            if (!$this->containerCacheCreated) {
-                $createResponse = $this->client->createCache($this->cachePrefix . 'container');
-                if ($createResponse->asSuccess()) {
-                    $this->containerCacheCreated = true;
-                } elseif ($createResponse->asError()) {
-                    try {
-                        // TODO: unify logging interface and direct this message to the logfile.
-                        $this->getLogger('momento_cache')->error(
-                            "Error getting Momento client: " . $createResponse->asError()->message()
-                        );
-                    } catch(ContainerNotInitializedException $e) {
-                        // we don't have access to getLogger() until the container is initialized
-                    }
-                }
-            }
         }
         return $this->client;
     }
