cd0b797fa3a75de4b62dc68e80dce8e1fbe5a023
Pete Gautier <pete@momentohq.com>
2023-11-17 12:39:18 -0800

    chore: add num_grpc_channels and batch_size settings

--- src/Client/MomentoClientFactory.php
+++ src/Client/MomentoClientFactory.php
@@ -12,31 +12,38 @@ class MomentoClientFactory {
     private $authProvider;
     private $client;
     private $forceNewChannel;
+    private $numGrpcChannels;
     private $logFile;
 
     public function __construct() {
         $settings = Settings::get('momento_cache', []);
         $authToken = array_key_exists('api_token', $settings) ?
             $settings['api_token'] : getenv("MOMENTO_API_TOKEN");
+        $this->authProvider = new StringMomentoTokenProvider($authToken);
         $this->forceNewChannel = array_key_exists('force_new_channel', $settings) ?
             $settings['force_new_channel'] : true;
-        $this->authProvider = new StringMomentoTokenProvider($authToken);
         $this->logFile =
             array_key_exists('logfile', $settings) ? $settings['logfile'] : null;
+        $this->numGrpcChannels =
+            array_key_exists('num_grpc_channels', $settings) ? $settings['num_grpc_channels'] : 1;
     }
 
     public function get() {
+        if ($this->client) {
+            return $this->client;
+        }
         $start = hrtime(true);
         $config = Laptop::latest();
         $config = $config->withTransportStrategy(
             $config->getTransportStrategy()->withGrpcConfig(
-                $config->getTransportStrategy()->getGrpcConfig()->withForceNewChannel($this->forceNewChannel)
+                $config
+                    ->getTransportStrategy()
+                    ->getGrpcConfig()
+                    ->withForceNewChannel($this->forceNewChannel)
+                    ->withNumGrpcChannels($this->numGrpcChannels)
             )
         );
-
-        if (!$this->client) {
-            $this->client = new CacheClient($config, $this->authProvider, 30);
-        }
+        $this->client = new CacheClient($config, $this->authProvider, 30);
         if ($this->logFile) {
             $totalTimeMs = (hrtime(true) - $start) / 1e6;
             $mt = microtime(true);
