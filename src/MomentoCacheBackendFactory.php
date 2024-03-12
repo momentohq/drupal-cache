5d841f16c1b573949bc9ba0acf287d21629daf30
rishtigupta <rishti@momentohq.com>
2024-03-12 15:27:12 -0700

    feat: update MomentoBackendFactory.php code based on phpcs results

--- src/MomentoCacheBackendFactory.php
+++ src/MomentoCacheBackendFactory.php
@@ -8,54 +8,55 @@ use Drupal\Core\Site\Settings;
 use Drupal\momento_cache\Client\MomentoClientFactory;
 
 /**
- *
+ * Defines the MomentoCacheBackendFactory service.
+ * Creates cache backend instances for the Momento cache system.
  */
 class MomentoCacheBackendFactory implements CacheFactoryInterface {
 
-    private $momentoFactory;
-    private $checksumProvider;
+  private $momentoFactory;
+  private $checksumProvider;
 
-    private static $cacheName;
-    private $client;
-    private $backends = [];
+  private static $cacheName;
+  private $client;
+  private $backends = [];
 
-    /**
-     *
-     */
-    public function __construct(
+  /**
+   * MomentoCacheBackendFactory constructor.
+   */
+  public function __construct(
         MomentoClientFactory $momento_factory,
         CacheTagsChecksumInterface $checksum_provider
     ) {
-        $this->momentoFactory = $momento_factory;
-        $this->checksumProvider = $checksum_provider;
-        $this->client = $this->momentoFactory->get();
-        $settings = Settings::get('momento_cache', []);
-        static::$cacheName = array_key_exists('cache_name', $settings) ?
+    $this->momentoFactory = $momento_factory;
+    $this->checksumProvider = $checksum_provider;
+    $this->client = $this->momentoFactory->get();
+    $settings = Settings::get('momento_cache', []);
+    static::$cacheName = array_key_exists('cache_name', $settings) ?
             $settings['cache_name'] : getenv("MOMENTO_CACHE_NAME");
+  }
+
+  /**
+   * Gets the configured cache name.
+   */
+  public static function getCacheName() {
+    return static::$cacheName ?? '';
+  }
+
+  /**
+   * Gets a cache backend instance for the specified bin.
+   */
+  public function get($bin) {
+    if (array_key_exists($bin, $this->backends)) {
+      return $this->backends[$bin];
     }
 
-    /**
-     *
-     */
-    public static function getCacheName() {
-        return static::$cacheName ?? '';
-    }
-
-    /**
-     *
-     */
-    public function get($bin) {
-        if (array_key_exists($bin, $this->backends)) {
-            return $this->backends[$bin];
-        }
-
-        $backend = new MomentoCacheBackend(
-            $bin,
-            $this->client,
-            $this->checksumProvider
-        );
-        $this->backends[$bin] = $backend;
-        return $backend;
-    }
+    $backend = new MomentoCacheBackend(
+          $bin,
+          $this->client,
+          $this->checksumProvider
+      );
+    $this->backends[$bin] = $backend;
+    return $backend;
+  }
 
 }
