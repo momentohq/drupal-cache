70f2e44d2475de14c562521132b4f952c98eaca1
rishtigupta <rishti@momentohq.com>
2024-03-12 16:15:25 -0700

    fix: update MomentoCacheBackendTest.php based on phpcs

--- tests/src/Kernel/MomentoCacheBackendTest.php
+++ tests/src/Kernel/MomentoCacheBackendTest.php
@@ -18,6 +18,12 @@ class MomentoCacheBackendTest extends GenericCacheBackendUnitTestBase {
    * @var array
    */
   protected static $modules = ['system', 'momento_cache'];
+
+  /**
+   * The cache name.
+   *
+   * @var string
+   */
   private $cacheName;
 
   /**
@@ -65,15 +71,15 @@ class MomentoCacheBackendTest extends GenericCacheBackendUnitTestBase {
   // tested is an extra credit exercise. They'd save us storage
   // for items that explicitly had their expiry set, but that seems
   // to be rare in Drupal usage and is redundant anyhow. Will revisit when
-  //  time permits.
-  //    public function testTtl() {
-  //        $backend = $this->getCacheBackend();
-  //        $backend->set('test1', 1, time() + 1);
-  //        $backend->set('test2', 1);
-  //        sleep(3);
-  //        $this->assertFalse
-  //      ($backend->get('test1'), 'Cache id test1 expired.');
-  //        $this->assertNotFalse
-  //      ($backend->get('test2'), 'Cache id test2 still exists.');
-  //    }
+  // time permits.
+  // public function testTtl() {
+  //  $backend = $this->getCacheBackend();
+  //  $backend->set('test1', 1, time() + 1);
+  //  $backend->set('test2', 1);
+  //  sleep(3);
+  //  $this->assertFalse
+  //  ($backend->get('test1'), 'Cache id test1 expired.');
+  //  $this->assertNotFalse
+  //  ($backend->get('test2'), 'Cache id test2 still exists.');
+  // }
 }
