6cd15a66fda929d1865b4462e316a5d431d0cc33
rishtigupta <rishti@momentohq.com>
2024-03-13 13:25:15 -0700

    fix: update MomentoClientFactory based on phpcs results

--- src/Client/MomentoClientFactory.php
+++ src/Client/MomentoClientFactory.php
@@ -7,51 +7,96 @@ use Momento\Auth\StringMomentoTokenProvider;
 use Momento\Cache\CacheClient;
 use Momento\Config\Configurations\Laptop;
 
+/**
+ * Momento client factory.
+ */
 class MomentoClientFactory {
 
-    private $authProvider;
-    private $client;
-    private $clientTimeoutMsec;
-    private $forceNewChannel;
-    private $numGrpcChannels;
-    private $logFile;
+    /**
+     * The authentication provider.
+     *
+     * @var \Momento\Auth\StringMomentoTokenProvider
+     */
+  private $authProvider;
 
-    public function __construct() {
-        $settings = Settings::get('momento_cache', []);
-        $authToken = array_key_exists('api_token', $settings) ?
+    /**
+     * The cache client.
+     *
+     * @var \Momento\Cache\CacheClient
+     */
+  private $client;
+
+    /**
+     * The client timeout in milliseconds.
+     *
+     * @var int
+     */
+  private $clientTimeoutMsec;
+
+    /**
+     * Whether to force a new channel.
+     *
+     * @var bool
+     */
+  private $forceNewChannel;
+
+    /**
+     * The number of gRPC channels.
+     *
+     * @var int
+     */
+  private $numGrpcChannels;
+
+    /**
+     * The log file.
+     *
+     * @var string
+     */
+  private $logFile;
+
+  /**
+   * MomentoClientFactory constructor.
+   */
+  public function __construct() {
+    $settings = Settings::get('momento_cache', []);
+    $authToken = array_key_exists('api_token', $settings) ?
             $settings['api_token'] : getenv("MOMENTO_API_TOKEN");
-        $this->authProvider = new StringMomentoTokenProvider($authToken);
-        $this->forceNewChannel = array_key_exists('force_new_channel', $settings) ?
-            $settings['force_new_channel'] : true;
-        $this->logFile =
-            array_key_exists('logfile', $settings) ? $settings['logfile'] : null;
-        $this->numGrpcChannels =
+    $this->authProvider = new StringMomentoTokenProvider($authToken);
+    $this->forceNewChannel = array_key_exists('force_new_channel', $settings) ?
+            $settings['force_new_channel'] : TRUE;
+    $this->logFile =
+            array_key_exists('logfile', $settings) ? $settings['logfile'] : NULL;
+    $this->numGrpcChannels =
             array_key_exists('num_grpc_channels', $settings) ? $settings['num_grpc_channels'] : 1;
-        $this->clientTimeoutMsec =
+    $this->clientTimeoutMsec =
             array_key_exists('client_timeout_msec', $settings) ? $settings['client_timeout_msec'] : 15000;
-    }
+  }
 
-    public function get() {
-        if ($this->client) {
-            return $this->client;
-        }
-        $start = hrtime(true);
-        $config = Laptop::latest();
-        $config = $config->withTransportStrategy(
-            $config->getTransportStrategy()->withGrpcConfig(
-                $config
-                    ->getTransportStrategy()
-                    ->getGrpcConfig()
-                    ->withForceNewChannel($this->forceNewChannel)
-                    ->withNumGrpcChannels($this->numGrpcChannels)
-            )
-        )->withClientTimeout($this->clientTimeoutMsec);
-        $this->client = new CacheClient($config, $this->authProvider, 30);
-        if ($this->logFile) {
-            $totalTimeMs = (hrtime(true) - $start) / 1e6;
-            $mt = microtime(true);
-            @error_log("[$mt] Instantiated cache client [$totalTimeMs ms]\n", 3, $this->logFile);
-        }
-        return $this->client;
+  /**
+   * Get the momento client.
+   */
+  public function get() {
+    if ($this->client) {
+      return $this->client;
     }
+    $start = hrtime(TRUE);
+    $config = Laptop::latest();
+    $config = $config->withTransportStrategy(
+          $config->getTransportStrategy()->withGrpcConfig(
+              $config
+                ->getTransportStrategy()
+                ->getGrpcConfig()
+                ->withForceNewChannel($this->forceNewChannel)
+                ->withNumGrpcChannels($this->numGrpcChannels)
+          )
+      )->withClientTimeout($this->clientTimeoutMsec);
+    $this->client = new CacheClient($config, $this->authProvider, 30);
+    if ($this->logFile) {
+      $totalTimeMs = (hrtime(TRUE) - $start) / 1e6;
+      $mt = microtime(TRUE);
+      @error_log("[$mt] Instantiated cache client [$totalTimeMs ms]\n", 3, $this->logFile);
+    }
+    return $this->client;
+  }
+
 }
